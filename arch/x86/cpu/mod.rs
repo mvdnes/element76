b84d0bf81b9affad34d2dd3e9880f8a361264782
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2014-07-03 09:14:49 +0200

    Packed arguments of isr_handler in a struct

--- arch/x86/cpu/mod.rs
+++ arch/x86/cpu/mod.rs
@@ -5,6 +5,14 @@ mod timer;
 
 static IRQ_OFFSET: u8 = 0x20;
 
+#[repr(C)]
+pub struct InterruptArguments {
+	_ds: u32, _edi: u32, _esi: u32, _ebp: u32, _esp: u32, _ebx: u32, _edx: u32, _ecx: u32, _eax: u32,
+	interrupt_number: u32,
+	error_code: u32,
+	_eip: u32, _cs: u32, _eflags: u32, _useresp: u32, _ss: u32,
+}
+
 pub fn idle()
 {
 	unsafe
@@ -42,16 +50,15 @@ pub fn setup()
 	timer::set_interval(50);
 }
 
-
 #[no_mangle]
-pub fn isr_handler(_ds: u32, _edi:u32, _esi:u32, _ebp:u32, _esp:u32, _ebx:u32, _edx:u32, _ecx:u32, _eax:u32, ino: u32, ec:u32, _eip:u32, _cs:u32, _eflags:u32, _useresp:u32, _ss:u32)
+pub extern "C" fn isr_handler(args: InterruptArguments)
 {
-	::kernel::interrupts::handle_interrupt(ino, ec);
+	::kernel::interrupts::handle_interrupt(args.interrupt_number, args.error_code);
 
 	// Ack IRQ
-	if ino >= (IRQ_OFFSET as u32)
+	if args.interrupt_number >= (IRQ_OFFSET as u32)
 	{
-		pic::acknowledge_irq(ino as u8 - IRQ_OFFSET);
+		pic::acknowledge_irq(args.interrupt_number as u8 - IRQ_OFFSET);
 	}
 }
 
