1f880230dfca11fb7324c0462ac19aaa4d933087
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2014-08-28 12:57:43 +0200

    Removed some unused code

--- arch/x86/mmu/mod.rs
+++ arch/x86/mmu/mod.rs
@@ -12,18 +12,3 @@ pub fn setup()
 		placement_address = end;
 	}
 }
-
-pub fn kmalloc(size: u32, align: bool) -> u32
-{
-	let mut pa = unsafe { placement_address };
-	if align && (pa & 0xFFFFF000) != 0
-	{
-		pa &= 0xFFFFF000;
-		pa += 0x1000;
-	}
-	unsafe
-	{
-		placement_address += size;
-	}
-	return pa;
-}
