956facfc7973f455e5b660e336797f55ce841f9b
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-10-30 11:04:49 +0100

    Update path in downloadrustlibdir
    Another small update: use nightly if no argument is given

--- downloadrustlibdir.sh
+++ downloadrustlibdir.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-version=$1
+version=${1-nightly}
 
 TARGET=i686-unknown-linux-gnu
 URL="http://static.rust-lang.org/dist/rust-${version}-${TARGET}.tar.gz"
-TARPATH=rust-${version}-${TARGET}/rustc/lib/rustlib/${TARGET}/lib/
+TARPATH=rust-${version}-${TARGET}/rust-std-${TARGET}/lib/rustlib/${TARGET}/lib/
 COMPONENTS=6
 FOLDER=rustlibdir
 
